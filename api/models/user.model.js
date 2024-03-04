18eb011ba07ad06dcaf5dbcc978962a4bd3e94fc
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-03-04 18:33:07 +0530

    create signin api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,6 +5,11 @@ const userShema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
     password: {
         type: String,
         required: true,
