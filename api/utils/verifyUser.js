412c25d138bd899e00d17c6eb4f54077d8314e34
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-06-21 17:46:11 +0530

    completed update user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -4,9 +4,9 @@ import jwt from 'jsonwebtoken';
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
 
-    if(!token) return next(errorHandler(401, 'Unauthorized'));
+    if(!token) return next(errorHandler(401, 'Unauthorized go away'));
     jwt.verify(token, process.env.JWT_SECRET, (err, user) =>{
-        if(err) return next(errorHandler(403, 'Forhibidden'));
+        if(err) return next(errorHandler(403, 'Foribidden'));
 
         req.user = user;
         next();
