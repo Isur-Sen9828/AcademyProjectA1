102d33d2cc8d834a6c0a0db47a6a4cca1e88e92b
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-07-25 12:48:20 +0530

    changes on routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import About from './pages/about';
 import SignIn from './pages/Signin';
 import Header from './components/header';
 import PrivateRoute from './components/PrivateRoute';
-
+import CreateListing from './pages/createListing';
 
 export default function App() {
   return (
@@ -20,6 +20,7 @@ export default function App() {
       <Route path="/about" element={<About/>} />
       <Route element = {<PrivateRoute/>}>
       <Route path="/profile" element={<Profile/>} />
+      <Route path="/create-listing" element={<CreateListing/>}/>
       </Route>
     </Routes>
     </BrowserRouter>
