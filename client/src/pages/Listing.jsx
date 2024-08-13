5c0e5e4a58ee307d1b2712087a3109a0b71529b9
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-08-13 21:17:49 +0530

    adding the image slider

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,8 +1,59 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {Swiper, SwiperSlide} from 'Swiper/react';
+import SwiperCore from 'Swiper';
+import {Navigation} from 'swiper/modules';
+import 'swiper/css/bundle';
+
 
 export default function Listing() {
+  SwiperCore.use(Navigation);
+  const[listing, setListing] = useState(null);
+  const[loading, setLoading] = useState(true);
+  const[error, setError] = useState(false);
+  const params = useParams();
+
+  useEffect(()=>{
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    }
+    fetchListing();
+  },[params.listingId]);
+
   return (
-    <div>Listing</div>
+    <main>
+      {loading && <p className='text-center font-semibold'>Loading...</p> }
+      {error && <p className='text-center font-semibold text-red-700'>Something went wrong...</p> }
+      {listing && !loading && !error &&(
+        (
+          <Swiper navigation>
+          {listing.imageURLs.map((url) => (
+          
+          <SwiperSlide key={url}>
+            <div className="h-[500px]" 
+            style={{background:`url(${url}) center no-repeat`,backgroundSize:'cover',}}
+            ></div>
+            </SwiperSlide>
+          ))}
+          </Swiper>
+          )
+      )}
+      </main>
   )
 }
